cee31f4d13705d95cc3598659c353a222d1f321d
TryLessFold <artem_141999@mail.ru>
2019-11-30 20:52:33 +0000

    Try #2

--- installer.sh
+++ installer.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo "	Installing dependencies..."
-apt -y  install xfce4
-apt -y autoremove xfce4
 echo "	Installing i3..."
 apt -y install i3
 echo "	Installing lightdm..."
+apt -y install xfce4
 apt -y install lightdm
+dpkg-reconfigure lightdm
+apt -y autoremove xfce4
+dpkg-reconfigure lightdm
 

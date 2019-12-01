c946b53e078d9623d770296e268a4ce0bad90b8e
TryLessFold <artem_141999@mail.ru>
2019-12-01 02:27:23 +0000

    Fixed simple installer

--- installer.sh
+++ installer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+echo "	Installing xorg..."
+apt -y install xorg
 echo "	Installing i3..."
 apt -y install i3
 echo "	Installing lightdm..."
-apt -y install xfce4
 apt -y install lightdm
-dpkg-reconfigure lightdm
-apt -y autoremove xfce4
-dpkg-reconfigure lightdm
-
+echo "	Create lightdm config file..."
+touch /etc/lightdm/lightdm.conf.d/50-i3.conf
+printf "[SeatDefaults]\nuser-session=i3" >> /etc/lightdm/lightdm.conf.d/50-i3.conf 

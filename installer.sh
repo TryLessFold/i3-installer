0cabe8d16d250484845a4a9c7e87b1cfc430441d
TryLessFold <artem_141999@mail.ry>
2019-12-02 01:06:50 +0000

    Features

--- installer.sh
+++ installer.sh
@@ -8,4 +8,17 @@ echo "	Installing lightdm..."
 apt -y install lightdm
 echo "	Create lightdm config file..."
 touch /etc/lightdm/lightdm.conf.d/50-i3.conf
-printf "[SeatDefaults]\nuser-session=i3" >> /etc/lightdm/lightdm.conf.d/50-i3.conf 
+printf "[SeatDefaults]\nuser-session=i3" >> /etc/lightdm/lightdm.conf.d/50-i3.conf
+
+echo "	Configure home directory..."
+cp ./.config ~/ -r
+chown $(id -u):$(id -g) /home/$(id -un)/ -R
+
+echo "Installing plugins..."
+apt -y install 
+
+echo "Installing programms..."
+apt -y install telegram-desktop
+apt -y install thunderbird
+apt -y install chromium-browser
+
